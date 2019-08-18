13fe43a92e55eb5d82e0d89587205225f367f6c8
Joao Guilherme <jaogui2001@gmail.com>
2019-08-18 09:07:37 -0300

    Exercicio de fixação exceptions.

---
+++ ExercicioException/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ExercicioExcption.Entities;
+using ExercicioExcption.Entities.Exceptions;
+
+namespace ExercicioExcption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double initialBalance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw limit: ");
+                double wdLimit = double.Parse(Console.ReadLine());
+
+                Account account = new Account(number, holder, initialBalance, wdLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double withdraw = double.Parse(Console.ReadLine());
+                account.WithDraw(withdraw);
+                Console.WriteLine("New balance: $ " + account.Balance.ToString("F2"));
+            }
+            catch (AccountException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
